fc0ebe9d52fa8eb86bbcc3be676fefb2dfca804d
Milana <milkasmilks@mail.ru>
2021-11-23 17:29:27 +0300

    add counter

--- src/app.js
+++ src/app.js
@@ -5,36 +5,36 @@ import './style.css';
  * Приложение
  * @param store {Store} Состояние с действиями
  */
-function App({store}) {
-  return (
-    <div className='App'>
-      <div className='App__head'>
-        <h1>Приложение на чистом JS</h1>
-      </div>
-      <div className='Controls'>
-        <button onClick={() => store.createItem()}> Добавить</button>
-      </div>
-      <div className='App__center'>
-        <div className='List'>{store.getState().items.map(item =>
-          <div
-            key={item.code}
-            className={'List__item' + (item.selected ? ' List__item_selected' : '')}
-          >
-            <div className='Item' onClick={() => store.selectItem(item.code)}>
-              <div className='Item__number'>{item.code}</div>
-              <div className='Item__title'>{item.title}</div>
-              <div className='Item__actions'>
-                <button onClick={() => store.deleteItem(item.code)}>
-                  Удалить
-                </button>
-              </div>
-            </div>
-          </div>
-        )}
-        </div>
-      </div>
-    </div>
-  );
+function App({ store }) {
+	return (
+		<div className='App'>
+			<div className='App__head'>
+				<h1>Приложение на чистом JS</h1>
+			</div>
+			<div className='Controls'>
+				<button onClick={() => store.createItem()}> Добавить</button>
+			</div>
+			<div className='App__center'>
+				<div className='List'>{store.getState().items.map(item =>
+					<div
+						key={item.code}
+						className={'List__item' + (item.selected ? ' List__item_selected' : '')}
+					>
+						<div className='Item' onClick={() => store.selectItem(item.code)}>
+							<div className='Item__number'>{item.code}</div>
+							<div className='Item__title'>{item.title + (item.counter > 0 ? ' | Выделялся ' + item.counter + ' раз' : '')}</div>
+							<div className='Item__actions'>
+								<button onClick={() => store.deleteItem(item.code)}>
+									Удалить
+								</button>
+							</div>
+						</div>
+					</div>
+				)}
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
