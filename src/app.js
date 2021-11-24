06733159fc9a8ad905e94e60f2064a5700b45a26
Milana <milkasmilks@mail.ru>
2021-11-24 13:47:27 +0300

    fix formatting

--- src/app.js
+++ src/app.js
@@ -6,35 +6,35 @@ import './style.css';
  * @param store {Store} Состояние с действиями
  */
 function App({ store }) {
-	return (
-		<div className='App'>
-			<div className='App__head'>
-				<h1>Приложение на чистом JS</h1>
-			</div>
-			<div className='Controls'>
-				<button onClick={() => store.createItem()}> Добавить</button>
-			</div>
-			<div className='App__center'>
-				<div className='List'>{store.getState().items.map(item =>
-					<div
-						key={item.code}
-						className={'List__item' + (item.selected ? ' List__item_selected' : '')}
-					>
-						<div className='Item' onClick={() => store.selectItem(item.code)}>
-							<div className='Item__number'>{item.code}</div>
-							<div className='Item__title'>{item.title + (item.counter > 0 ? ' | Выделялся ' + item.counter + ' раз' : '')}</div>
-							<div className='Item__actions'>
-								<button onClick={() => store.deleteItem(item.code)}>
-									Удалить
-								</button>
-							</div>
-						</div>
-					</div>
-				)}
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className='App'>
+      <div className='App__head'>
+        <h1>Приложение на чистом JS</h1>
+      </div>
+      <div className='Controls'>
+        <button onClick={() => store.createItem()}> Добавить</button>
+      </div>
+      <div className='App__center'>
+        <div className='List'>{store.getState().items.map(item =>
+          <div
+            key={item.code}
+            className={'List__item' + (item.selected ? ' List__item_selected' : '')}
+          >
+            <div className='Item' onClick={() => store.selectItem(item.code)}>
+              <div className='Item__number'>{item.code}</div>
+              <div className='Item__title'>{item.title + (item.counter > 0 ? ' | Выделялся ' + item.counter + ' раз' : '')}</div>
+              <div className='Item__actions'>
+                <button onClick={() => store.deleteItem(item.code)}>
+                  Удалить
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -7,20 +7,20 @@ const root = document.getElementById("app");
 
 // Состояние приложения
 const store = new Store({
-	items: [
-		{ code: 1, title: 'Название элемента' },
-		{ code: 2, title: 'Некий объект' },
-		{ code: 3, title: 'Заголовок' },
-		{ code: 4, title: 'Короткое название' },
-		{ code: 5, title: 'Запись' },
-		{ code: 6, title: 'Пример названия' },
-		{ code: 7, title: 'Седьмой' }
-	]
+  items: [
+    { code: 1, title: 'Название элемента' },
+    { code: 2, title: 'Некий объект' },
+    { code: 3, title: 'Заголовок' },
+    { code: 4, title: 'Короткое название' },
+    { code: 5, title: 'Запись' },
+    { code: 6, title: 'Пример названия' },
+    { code: 7, title: 'Седьмой' }
+  ]
 });
 
 // Сообщаем реакту что и куда рендерить.
 store.subscribe(() => {
-	ReactDOM.render(<App store={store} />, root);
+  ReactDOM.render(<App store={store} />, root);
 });
 
 // Сообщаем реакту что и куда рендерить.
